8933deb49cc9243c54d97fefe58eb9c45af79d76
laudkyle <kyleaby1@gmail.com>
2024-02-28 18:52:25 -0800

    spoonacular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=20`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+
   return (
     <div>
-      <h1>Popular</h1>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;

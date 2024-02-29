feeb9f4a1edfb24f9e11998d59f4bb65a02db9ca
laudkyle <kyleaby1@gmail.com>
2024-02-28 18:23:05 -0800

    Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Home from "./pages/Home";
-
+import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Home />
+      <Pages />
     </div>
   );
 }

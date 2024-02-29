d6604061ac4c6947a6ae55d8e172fe3bb5b3e163
laudkyle <kyleaby1@gmail.com>
2024-02-29 04:20:40 +0000

    css

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -53,6 +53,11 @@ const Card = styled.div`
   overflow: hidden;
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 `;
 export default Popular;

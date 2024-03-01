b55c7bf1667a23269a869fbbf4672b4f2726544b
laudkyle <kyleaby1@gmail.com>
2024-03-01 15:37:38 +0000

    changes

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -34,7 +34,7 @@ function Cuisine() {
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(autofit, minmax(20rem, 1fr));
-  grid-gap: 3rem;
+  grid-gap: 1rem;
 `;
 const Card = styled.div`
     img{
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+
 function Searched() {
   let params = useParams();
   useEffect(() => {

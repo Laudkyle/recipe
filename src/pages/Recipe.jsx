c520670d2c91df775ab3a53c83b7047144503b48
laudkyle <kyleaby1@gmail.com>
2024-03-03 13:16:34 +0000

    recipe details

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,18 +3,19 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 function Recipe() {
-let params = useParams();
-const[details, setDetails] = useState({});
-useEffect(()=>{
-  
-})
-  const fetchDetails = async() =>{
+  let params = useParams();
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+  const fetchDetails = async () => {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
-    )
+    );
     const detailData = await data.json();
-setDetails(detailData);
-  }
+    setDetails(detailData);
+  };
   return <div>Recipe</div>;
 }
 

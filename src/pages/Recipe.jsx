145de34717b1f0cada0b1be3777e99ad016aac23
laudkyle <kyleaby1@gmail.com>
2024-03-05 18:09:57 +0000

    Info

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -42,10 +42,12 @@ const DetailsWrapper = styled.div`
 const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
-  background:white;
-  border:2px solid black;
-  margin-right:2rem;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
   font-weight: 600;
 `;
-c
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 export default Recipe;

a67490c2d3cb3cea9427d40c380ae0f9fb7c2640
laudkyle <kyleaby1@gmail.com>
2024-03-05 18:08:58 +0000

    button

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -23,20 +23,29 @@ const DetailsWrapper = styled.div`
   margin-top: 10rem;
   margin-bottom: 5rem;
   display: flex;
-  .active{
+  .active {
     background: linear-gradient(35deg, #494949, #313131);
     color: white;
   }
-  h2{
-    margin-bottom:2rem;
+  h2 {
+    margin-bottom: 2rem;
   }
-  li{
-    font-size:1.2rem;
-    line-height:2.5rem;
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
   }
-  ul{
-    margin-top:2rem;
+  ul {
+    margin-top: 2rem;
   }
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background:white;
+  border:2px solid black;
+  margin-right:2rem;
+  font-weight: 600;
+`;
+c
 export default Recipe;

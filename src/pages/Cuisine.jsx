fa9a054267a6015b7ea353cc4561401a1049294b
laudkyle <kyleaby1@gmail.com>
2024-03-06 16:03:37 +0000

    complete app

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -20,12 +20,17 @@ function Cuisine() {
   }, [params.type]);
 
   return (
-    <Grid>
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }} 
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
             <Link to={"/recipe/" + item.id}>
-              <img src={item.image}  />
+              <img src={item.image} />
               <h4>{item.title}</h4>
             </Link>
           </Card>
@@ -35,7 +40,7 @@ function Cuisine() {
   );
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 1rem;
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Home from "./Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,useLocation } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation();
   return (
-    <Routes>
+    <AnimatePresence>
+    <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
       <Route path="/searched/:searched" element={<Searched />} />
       <Route path="/recipe/:name" element={<Recipe />}/>
     </Routes>
+    </AnimatePresence>
   );
 }
 

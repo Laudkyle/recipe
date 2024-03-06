7322166aa44dadf2fce793a2c1cdd91b56c4d0ad
laudkyle <kyleaby1@gmail.com>
2024-03-06 15:51:28 +0000

    Scrumptious

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Nav>
-        <Logo to={'/'} />
+      <GiKnifeFork />
+        <Logo to={'/'}>Scrumptious</Logo>
       </Nav>
         <Search />
         <Categories />
